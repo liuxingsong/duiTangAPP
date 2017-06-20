4fcd6389159d3769cb70d0c630eea4b35056592b
AsmallBox <xingsong2017@126.com>
2017-06-20 21:46:13 +0800

    lxs commit

--- src/components/gridList.js
+++ src/components/gridList.js
@@ -28,7 +28,8 @@ import { hashHistory } from 'react-router'
    	render(){
   		return <Grid data={this.state.gridData} columnNum={4} hasLine={false}
             renderItem={(dataItem, index) => (
-  	        <div style={{ margin: '0.16rem', textAlign: 'center' }} onClick={()=>this.goToDetail(dataItem.id,dataItem.value,dataItem.name)}>
+  	        <div style={{ margin: '0.16rem', textAlign: 'center' }}
+            onClick={()=>this.goToDetail(dataItem.id,dataItem.value,dataItem.name)}>
   	        	<img src="" style={{ width: '80%', margin: '0.12rem' }} alt="icon" />
   	          <div style={{ padding: '0.08rem' }}>
   	            <span>{dataItem.name}</span>
